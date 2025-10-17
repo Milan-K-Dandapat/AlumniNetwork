0537f62c33ef08a8737389514dbd569a07f46ae4
Milan Kumar Dandapat <milankumar07@outlook.com>
2025-10-17 20:44:44 +0530

    update statsController

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,23 +1,24 @@
 import Alumni from '../models/Alumni.js';
 import Teacher from '../models/Teacher.js';
-// We will add the Event models here later
+import Event from '../models/Event.js'; // 👈 1. IMPORT the Event model
 
 // @desc    Get key network statistics (user and event counts)
 // @route   GET /api/stats
 // @access  Public
 export const getNetworkStats = async (req, res) => {
     try {
-        // Use Promise.all to run the database queries in parallel for maximum speed
-        const [alumniCount, facultyCount] = await Promise.all([
+        // Use Promise.all to run all database queries in parallel for maximum speed
+        const [alumniCount, facultyCount, eventCount] = await Promise.all([ // 👈 2. Add eventCount
             Alumni.countDocuments({ isVerified: true }),
-            Teacher.countDocuments({ isVerified: true })
-            // We will add the event count query here later
+            Teacher.countDocuments({ isVerified: true }),
+            Event.countDocuments({}) // 👈 3. Add the query to count ALL events
         ]);
 
-        // Send the counts back in a clean JSON object
+        // Send all the counts back in a clean JSON object
         res.status(200).json({
             alumni: alumniCount,
             faculty: facultyCount,
+            totalEvents: eventCount, // 👈 4. Add the new event count to the response
         });
 
     } catch (error) {
