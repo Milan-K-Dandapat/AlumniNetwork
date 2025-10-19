9474a17039f7890858c846b96d0b0c9e90dce682
Milan Kumar Dandapat <milankumar07@outlook.com>
2025-10-19 22:49:36 +0530

    update server.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import auth from '../middleware/auth.js';
+import { protect as auth } from '../middleware/auth.js';
 import Alumni from '../models/Alumni.js';
 import Teacher from '../models/Teacher.js'; 
 
